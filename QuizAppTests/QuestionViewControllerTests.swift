02e3b350c44edd5587c76bdca02fa168ea4de702
Jero Sánchez <jsanchez@loycus.com>
2020-04-17 18:48:59 +0200

    Create Question view controller

---
+++ QuizAppTests/QuestionViewControllerTests.swift
@@ -0,0 +1,40 @@
+import Foundation
+import XCTest
+@testable import QuizApp
+
+class QuestionViewControllerTests: XCTestCase {
+
+    func test_viewDidLoad_rendersQuestionHeaderTest() {
+        let sut = QuestionViewController(question: "Q1", options: [])
+        
+        sut.loadViewIfNeeded()
+        
+        XCTAssertEqual(sut.headerLabel.text, "Q1")
+    }
+    
+    func test_viewDidLoad_withNoOptions_rendersZeroOptions() {
+        let sut = QuestionViewController(question: "Q1", options: [])
+
+        sut.loadViewIfNeeded()
+
+        XCTAssertEqual(sut.tableView.numberOfRows(inSection: 0), 0)
+    }
+    
+    func test_viewDidLoad_withOneOption_rendersOneOption() {
+        let sut = QuestionViewController(question: "Q1", options: ["A1"])
+
+        sut.loadViewIfNeeded()
+
+        XCTAssertEqual(sut.tableView.numberOfRows(inSection: 0), 1)
+    }
+    
+    func test_viewDidLoad_withOneOption_rendersOneOptionText() {
+        let sut = QuestionViewController(question: "Q1", options: ["A1"])
+        
+        sut.loadViewIfNeeded()
+        let indexPath = IndexPath(row: 0, section: 0)
+        let cell = sut.tableView.dataSource?.tableView(sut.tableView, cellForRowAt: indexPath)
+
+        XCTAssertEqual(cell?.textLabel?.text, "A1")
+    }
+}
