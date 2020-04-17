c0d0e3aad8b3791621b908f409ad6380bd337c07
Jero Sánchez <jsanchez@loycus.com>
2020-04-17 17:26:02 +0200

    Initial Commit

---
+++ QuizApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QuizApp
+//
+//  Created by Jero Sánchez on 17/04/2020.
+//  Copyright © 2020 Jero Sánchez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
