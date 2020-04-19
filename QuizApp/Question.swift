9cec327b46d0c6e0ddd586c06eaf881e7b31174d
Jero Sánchez <jsanchez@loycus.com>
2020-04-19 19:48:25 +0200

    Create Question type

---
+++ QuizApp/Question.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+enum Question<T: Hashable>: Hashable {
+    case singleAnswer(T)
+    case multipleAnswer(T)
+    
+    func hash(into hasher: inout Hasher) {
+       switch self {
+        case .singleAnswer(let value):
+            hasher.combine(value)
+        case .multipleAnswer(let value):
+            hasher.combine(value)
+        }
+    }
+    
+    static func ==(lhs: Question<T>, rhs: Question<T>) -> Bool {
+        switch (lhs, rhs) {
+        case (.singleAnswer(let a), .singleAnswer(let b)):
+            return a == b
+        case (.multipleAnswer(let a), .multipleAnswer(let b)):
+            return a == b
+        default:
+            return false
+        }
+    }
+}
